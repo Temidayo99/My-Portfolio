ae6d5ca44d4ec5aa3743d9d117103f244789ec34
Temidayo99 <oyelamitemidayo99@gmail.com>
2021-07-29 05:08:47 +0100

    modified files

--- js/script.js
+++ js/script.js
@@ -7,14 +7,34 @@ function toggleMenu() {
         menu.classList.remove("active");
          
         // adds the menu (hamburger) icon
-        toggle.querySelector("a").innerHTML = "<i class=’fas fa-bars’></i>";
+        toggle.querySelector("a").innerHTML = "<i class=’fa fa-bars’></i>";
     } else {
         menu.classList.add("active");
          
         // adds the close (x) icon
-        toggle.querySelector("a").innerHTML = "<i class=’fas fa-times’></i>";
+        toggle.querySelector("a").innerHTML = "<i class=’fa fa-close’></i>";
     }
 }
  
 /* Event Listener */
 toggle.addEventListener("click", toggleMenu, false);
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
